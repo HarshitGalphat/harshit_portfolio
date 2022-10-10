ef175eafabbb1bafb2a383c6b1e5fc8b213d08a8
Harshit galphat <94209852+HarshitGalphat@users.noreply.github.com>
2022-10-10 17:00:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // let btn=document.getElementById('btn');
 // btn.addEventListener("click", clicked());
-function clicked(){
-    alert("Quiry sent!");
-};
+// function clicked(){
+//     alert("Quiry sent!");
+// };
